949e1409f2bbd3b93b0febc4840d1631d4041d1c
Dan Bai <dbai5@wisc.edu>
2018-01-27 23:25:29 -0600

    Assess include huuuuuuuman

--- App/Components/Assess.js
+++ App/Components/Assess.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {
   View,
   Text,
+  Image,
   Button,
   TouchableOpacity,
   StyleSheet,
@@ -28,7 +29,11 @@ class Assess extends React.Component {
     let { navigate } = this.props.navigation;
     return (
       <View style = {styles.homeContainer}>
-        <Text style={styles.text}> Insert Blue Human Here </Text>
+        <Image style={styles.image} source={require('../Src/_Image/Human.jpg')}/>
+        <View style={styles.head}/>
+        <View style={styles.torso}/>
+        <View style={styles.legg}/>
+
       </View>
     );
   }
@@ -37,12 +42,48 @@ class Assess extends React.Component {
 const styles = StyleSheet.create({
   homeContainer: {
     flex: 1,
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
   },
+  image: {
+     flex:1,
+     resizeMode: 'contain',
+     borderWidth: 2,
+     borderColor: '#000',
+  },
   text: {
+    position: 'absolute',
     fontWeight: 'bold',
     fontSize: 40,
+  },
+  head: {
+    position: 'absolute',
+    top: '2%',
+    width: '20%',
+    height: '15%',
+    borderRadius: 10,
+    borderWidth: 1,
+    backgroundColor: 'transparent',
+  },
+  torso: {
+    position: 'absolute',
+    top: '15%',
+    width: '30%',
+    height: '35%',
+    borderRadius: 10,
+    borderWidth: 1,
+    backgroundColor: 'transparent',
+  },
+  legg: {
+    position: 'absolute',
+    bottom: '1%',
+    width: '30%',
+    height: '50%',
+    borderRadius: 10,
+    borderRadius:
+    backgroundColor: 'transparent',
+
   }
 });
 
