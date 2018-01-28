1e379cf3eedc13cdce9aeb08793bc47889c4b7b5
Dan Bai <dbai5@wisc.edu>
2018-01-28 09:54:01 -0600

    Merged Files

--- App/Components/Diagnosis.js
+++ App/Components/Diagnosis.js
@@ -48,17 +48,28 @@ class Item extends React.Component {
     });
   }
 
+	// this.handleImage.bind(this)
+	handleImage(){
+		switch(this.props.itemInfo.Severity){
+			case 'High': return('../Src/_Image/Human.jpg');
+			case 'Medium': return('../Src/_Image/Human.jpg');
+			case 'Low': return('../Src/_Image/Human.jpg');
+		}
+	}
+
+
 	render() {
 		return(
       <View style={styles.itemContainer}>
         <View style={styles.touchContainer}>
           <TouchableOpacity
-            style={styles.item}
+
+						style={styles.item}
             onPress={
               () => this.handleInfoClick()
             }
             >
-              <Text >{this.props.itemInfo.title}</Text>
+              <Text style={styles.textBig} >{this.props.itemInfo.title}</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.image}
@@ -67,10 +78,20 @@ class Item extends React.Component {
             }
             >
 
-          </TouchableOpacity>
+						{this.props.itemInfo.Severity == 'High' ?
+							<Image style={styles.image} source={require('../Src/_Image/sevTato.png')}/> :
+							<Image style={styles.image} source={require('../Src/_Image/medTato.png')}/>
+						}
+
+
+
+
+					</TouchableOpacity>
         </View>
         <Animated.View style={ [styles.dropDown, {height: this.state.height}] }>
-          
+
+				<Text style={styles.textSmall}> {this.props.itemInfo.details} </Text>
+
         </Animated.View>
       </View>
 		);
@@ -80,30 +101,63 @@ class Item extends React.Component {
 class Diagnosis extends React.Component {
   render() {
     return(
-      <ScrollView style={{ flex: 1 }}>
-          <View style={{ flex: 1 }}>
-              {list.map((item, key) => {
-								return (
-                  <Item itemInfo={item} key={key}/>
-                )
-              })}
-          </View>
-      </ScrollView>
+			<View style={{flex:1, backgroundColor:'transparent'}}>
+        <View>
+          <Image
+            style={styles.background}
+            source={require('../Src/_Image/Background.png')}
+          />
+        </View>
+        <ScrollView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                {list.map((item, key) => {
+  								return (
+                    <Item itemInfo={item} key={key}/>
+                  )
+                })}
+            </View>
+        </ScrollView>
+      </View>
     );
   }
 }
 
-let list = [
-  {
-    title: 'hello',
-  },
-  {
-    title: 'potato',
-  },
-  {
-    title: 'go',
-  }
-];
+	let list = [
+
+	  {
+	    title: 'CTE (Chronic Traumatic Encephalopathy)',
+			details: 'Severity: High\n\n' +
+			'Symptoms: Memory Problems, Personality Changes (Depression or Suicidal Thoughts), Aggression, Difficulty Balancing\n\n' +
+			'Description: Often caused by taking multiple hits to the head without losing consciousness.\n\n' +
+			'Source: Ramsay Law Firm, Alzheimers Association',
+			Severity: 'High',
+		},
+	  {
+	    title: 'Concussions or Traumatic Brain Injury',
+			details: 'Severity: Medium\n\n' +
+			'Symptoms: Slurred Speech, Headache, Falling Unconscious, Confusion, Amnesia, Dizziness, Fatigue\n\n' +
+			'Description: Brain injury caused by the brain being jostled in the skull. May lead to bleeding in or around the brain which can occur immeditately or shortly after the incident.\n\n' +
+			'Source: Ramsay Law Firm, Mayo Clinic',
+			Severity: 'Medium',
+	  },
+	  {
+	    title: 'Intracranial Hematomas',
+			details: 'Severity: High\n\n' +
+			'Symptoms: Paralysis, Fainting, Difficulty Speaking, Headaches, Seizures, Sensitivity to Light\n\n' +
+			'Description: Blood building up in the area around the brain, creating pressure on the skull.\n\n' +
+			'Source: Ramsay Law Firm, Mayo Clinic',
+			Severity: 'High',
+	  },
+		{
+			title: 'Cerebral Contusions',
+			details: 'Severity: High\n\n' +
+			'Symptoms: Nausea, Dizziness, Headaches, Seizures\n\n' +
+			'Description: Bruising of the brain. Caused by taking hits to the head. Shares many same causes as concussions, but with more severe results.\n\n' +
+			'Ramsay Law Firm, SpinalCord.com',
+			Severity: 'High',
+		}
+
+	];
 
 const styles = StyleSheet.create({
   itemContainer: {
@@ -116,28 +170,60 @@ const styles = StyleSheet.create({
     width:  '100%',
 		borderRadius: 10,
 		borderWidth: 1,
-		backgroundColor: '#bd632f',
+		backgroundColor: '#72A0C1',
 		height: require('Dimensions').get('window').width/3,
 		flexDirection: 'row',
   },
   item: {
     borderWidth:1,
 		flex: 2,
-		backgroundColor: 'orange',
+		backgroundColor: '#334893',
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
+		alignItems: 'stretch',
+		justifyContent: 'center',
   },
   image: {
     flex: 1,
     height: '100%',
-    backgroundColor: 'green',
+    backgroundColor: '#556bba',
     borderTopRightRadius: 10,
     borderBottomRightRadius: 10,
   },
   dropDown: {
-    backgroundColor: 'green',
-		width: '95%'
+    backgroundColor: '#CCCCFF',
+		width: '95%',
+		overflow:"hidden",
+  },
+
+	textBig: {
+		//marginTop: '13.5%',
+		padding: 5,
+		color: '#fff',
+		fontWeight: 'bold',
+		fontSize: 20,
+		overflow: 'hidden',
+		alignItems:'stretch'
+
+	},
+
+	textSmall: {
+		padding: 5,
+		//fontWeight: 'bold',
+		justifyContent: 'center',
+    fontSize: 18,
+		overflow: 'hidden',
+	},
+
+	background: {
+    top:0,
+    left:0,
+    height:require('Dimensions').get('window').height,
+    width:require('Dimensions').get('window').width,
+    position:'absolute',
+    resizeMode:'repeat',
   }
+
 });
 
 export default Diagnosis;
--- App/Components/Prepare.js
+++ App/Components/Prepare.js
@@ -7,6 +7,41 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
+import {
+  KeyboardAwareScrollView
+} from 'react-native-keyboard-aware-scroll-view';
+import tcomb from 'tcomb-form-native';
+
+const Form = tcomb.form.Form;
+
+const Sport = tcomb.enums({
+  Football: 'Football',
+  Soccer: 'Soccer',
+  Swimming: 'Swimming',
+  Tennis: 'Tennis',
+  Track: 'Track',
+  'Cross Country': 'Cross Country',
+  Boxing: 'Boxing',
+});
+
+const Duration = tcomb.enums({
+  Day: "Day",
+  Week: "Week",
+  Month: "Month",
+  Year: "Year",
+});
+
+const Survey = tcomb.struct({
+  Sport: Sport,
+  Experience: Duration,
+  Number: tcomb.Number,
+})
+
+let options = {
+    fields: {
+
+    },
+};
 
 class Prepare extends React.Component {
   constructor(props) {
@@ -27,9 +62,30 @@ class Prepare extends React.Component {
   render() {
     let { navigate } = this.props.navigation;
     return (
-      <View style = {styles.homeContainer}>
-        <Text style={styles.text}> Prepare Stuff Goes Here </Text>
+      <KeyboardAwareScrollView
+      style={{ backgroundColor: '#ffffff' }}
+      resetScrollToCoords={{ x: 0, y: 0 }}
+      contentContainerStyle={ styles.container }
+      scrollEnabled={true}
+      >
+      <View style={{ flex: 4, justifyContent: 'center' }}>
+        <Form
+          ref={(ref) => this._form = ref}
+          type={Survey}
+          options={options}
+        />
       </View>
+      <View style={ styles.buttonBox }>
+        <TouchableOpacity
+           style={styles.button}
+           onPress={
+             () => Console.log("Submit")
+           }
+         >
+           <Text style={styles.text}> Check </Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAwareScrollView>
     );
   }
 }
@@ -40,10 +96,33 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  buttonBox: {
+    flex: 1,
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#911',
+    borderRadius: 10,
+    borderWidth: 0.5,
+    borderColor: '#000',
+    width: 100,
+    height: 50,
+  },
   text: {
+    color: 'white',
     fontWeight: 'bold',
-    fontSize: 40,
-  }
+    fontSize: 30,
+  },
 });
 
 export default Prepare;
