c0a40e028bfdf9ea2ba317e85ab4bdf3c89594c4
Dan Bai <dbai5@wisc.edu>
2018-01-28 05:19:53 -0600

    List view works now

--- App/Components/Diagnosis.js
+++ App/Components/Diagnosis.js
@@ -10,16 +10,16 @@ import {
 	LayoutAnimation,
 	StyleSheet,
 	Platform,
-	UIManager
+	UIManager,
+  ScrollView,
 } from 'react-native';
 
-class Diagnosis extends React.Component {
+class Item extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			show: false,
-			showImage: false,
-			h: new Animated.Value(0)
+      show: false,
+      height: new Animated.Value(0),
 		}
 		if (Platform.OS === 'android') {
 			UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
@@ -30,115 +30,114 @@ class Diagnosis extends React.Component {
 		<View style={styles.seperator} />
 	}
 
-	handleImageClick(){
-		this.setState({
-			showImage: !this.state.showImage
-		});
-	}
-
-	handleInfoClick(){
-		this.setState({
-			show: !this.state.show
-		}, () => {
-			if(this.state.show){
-				Animated.timing(this.state.h, {
-					toValue: require('Dimensions').get('window').width/2,
-					duration: 500
-				}).start()
-			}else{
-				Animated.timing(this.state.h, {
-					toValue: 0,
-					duration: 500
-				}).start()
-			};
-		});
-	}
+  handleInfoClick(){
+    this.setState({
+      show: !this.state.show
+    }, () => {
+      if(this.state.show){
+        Animated.timing(this.state.height, {
+          toValue: require('Dimensions').get('window').width,
+          duration: 500
+        }).start()
+      }else{
+        Animated.timing(this.state.height, {
+          toValue: 0,
+          duration: 500
+        }).start()
+      };
+    });
+  }
 
 	render() {
-		if(this.state.showImage){
-			return (
-			<View style={{ flex: 1 }}>
-				{/* image stuff happens here
-        handle later */}
-			</View>
-			);
-		}
 		return(
-  		<View style={{ flex: 1, paddingTop: 5, padding: 10, alignItems: 'center' }}>
-  			<View style={ styles.smallWindow }>
-  				<TouchableOpacity style={ styles.listInfo } onPress={ ()=>this.handleInfoClick() }>
-  					<View style={ styles.window }>
-  						{/* <Text style={ styles.itle }>{this.props.listInfo.title}</Text> */}
-  					</View>
-  				</TouchableOpacity>
-  				<TouchableOpacity style={[ styles.mapInfo, { zIndex:1} ]} onPress={ ()=>this.handleImageClick() }>
-  					<View style={{flex: 1, backgroundColor: 'gray'}}></View>
-  				</TouchableOpacity>
-  			</View>
-  			<Animated.View style={ [styles.dropDown, {height: this.state.h}] }>
-  				<Text style={{overflow: 'hidden'}}>Description</Text>
-  			</Animated.View>
-  		</View>
+      <View style={styles.itemContainer}>
+        <View style={styles.touchContainer}>
+          <TouchableOpacity
+            style={styles.item}
+            onPress={
+              () => this.handleInfoClick()
+            }
+            >
+              <Text >{this.props.itemInfo.title}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.image}
+            onPress={
+              () => {}
+            }
+            >
+
+          </TouchableOpacity>
+        </View>
+        <Animated.View style={ [styles.dropDown, {height: this.state.height}] }>
+          
+        </Animated.View>
+      </View>
 		);
 	}
 }
 
+class Diagnosis extends React.Component {
+  render() {
+    return(
+      <ScrollView style={{ flex: 1 }}>
+          <View style={{ flex: 1 }}>
+              {list.map((item, key) => {
+								return (
+                  <Item itemInfo={item} key={key}/>
+                )
+              })}
+          </View>
+      </ScrollView>
+    );
+  }
+}
+
+let list = [
+  {
+    title: 'hello',
+  },
+  {
+    title: 'potato',
+  },
+  {
+    title: 'go',
+  }
+];
+
 const styles = StyleSheet.create({
-	window: {
-		backgroundColor: '#bd632f',
-		zIndex: 1,
-		flex: 1
-	},
-	returnButton: {
-		alignItems: 'center',
-		backgroundColor: '#DDDDDD',
-		zIndex: 1,
-		padding: 10
-	},
-	dropDown:{
-		flexDirection: 'row',
-		backgroundColor: '#d8973c',
-		width: '95%'
-	},
-	smallWindow: {
-		width:  '100%',
+  itemContainer: {
+    flex: 1,
+    paddingTop: 5,
+    padding: 10,
+    alignItems: 'center',
+  },
+  touchContainer: {
+    width:  '100%',
 		borderRadius: 10,
 		borderWidth: 1,
 		backgroundColor: '#bd632f',
 		height: require('Dimensions').get('window').width/3,
-		flexDirection: 'row'
-	},
-	title: {
-		textAlign: "center",
-		fontSize: 20
-	},
-	eventInfo: {
-		borderWidth:1,
-		zIndex: 0,
+		flexDirection: 'row',
+  },
+  item: {
+    borderWidth:1,
 		flex: 2,
-		overflow: 'hidden',
-		borderWidth: 0,
-		borderRadius: 10,
-		backgroundColor: '#bd632f'
-	},
-	mapInfo: {
-		flex: 1,
-		borderWidth: 0,
-		borderBottomRightRadius: 10,
-		borderTopRightRadius: 10,
-		overflow: 'hidden',
-		backgroundColor: '#bd632f'
-	},
-	text: {
-		textAlign: "center",
-		fontSize: 12
-	},
-	map: {
-		position: 'relative',
-		overflow: 'hidden',
-		zIndex: 1,
-		flex: 1, flexDirection: 'row'
-	}
+		backgroundColor: 'orange',
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+  },
+  image: {
+    flex: 1,
+    height: '100%',
+    backgroundColor: 'green',
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+  },
+  dropDown: {
+    backgroundColor: 'green',
+		width: '95%'
+  }
 });
 
 export default Diagnosis;
--- App/Navigator/TabNavigator.js
+++ App/Navigator/TabNavigator.js
@@ -1,11 +1,9 @@
 import { TabNavigator } from "react-navigation";
 
 import AppHome from '../Containers/Home';
-import Diagnosis from '../Containers/Diagnosis';
 
 const MainScreenNavigator = TabNavigator({
   Home: { screen: AppHome },
-  Diagnosis: { screen: Diagnosis },
 });
 
 MainScreenNavigator.navigationOptions = {
